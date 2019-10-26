f3dde7a9c35e075e4ba72f7aa7e7f5a53e945d2f
See Yang <seeyang24@gmail.com>
2019-10-26 10:30:44 -0500

    able to display boolean value in map for reviewtimesheet

--- src/components/ReviewRequest/ReviewRequest.js
+++ src/components/ReviewRequest/ReviewRequest.js
@@ -3,16 +3,31 @@ import {connect} from 'react-redux';
 
 class ReviewRequest extends Component {
 
+    state = {
+        is_approved: false,
+        deny_request: false
+    }
+
     componentDidMount() {
         this.getReviewTime();
     }
 
+    // sends dispatch to saga
     getReviewTime = () => {
         this.props.dispatch({
             type: 'FETCH_TIMESHEET_REVIEW',
             payload: this.props.match.params.id
         })
         console.log('get Review TIME', this.props.review)
+    }
+
+    // approve the request
+    handleApprove = () => {
+        this.setState({
+            ...this.state.is_approved,
+            is_approved: true
+        });
+        console.log('HANDLE APPROVE', this.state.is_approved)
     }
 
     render() {
@@ -27,7 +42,13 @@ class ReviewRequest extends Component {
                 <li>{time.wednesday} : {time.wednesday_hours}</li>
                 <li>{time.thursday} : {time.thursday_hours}</li>
                 <li>{time.friday} : {time.friday_hours}</li>
+                <li>Approved: {time.is_approved.toString()}</li>
                 <li>Total Hours: {time.total}</li>
+                <br />
+                <textarea></textarea>
+                <br />
+                <button onClick={this.handleApprove}>Approve</button>
+                <button>Deny</button>
                 </>
             )
         })
@@ -39,6 +60,7 @@ class ReviewRequest extends Component {
                 <ul>
                     {timeDetails}
                 </ul>
+                <p>{JSON.stringify(this.props.review)}</p>
             </div>
         )
     }
